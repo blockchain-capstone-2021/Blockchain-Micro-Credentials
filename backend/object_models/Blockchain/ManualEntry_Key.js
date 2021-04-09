bba1c6598d53ca73207aa86eb149ce4164fd94f7
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-09 17:43:54 +1000

    deleted object models

--- backend/object_models/Blockchain/ManualEntry_Key.js
+++
@@ -1,9 +0,0 @@
-class ManualEntry_Key{
-    constructor (studentId, unitId, semEnrol){
-        this._studentId = studentId
-        this._unitId = unitId
-        this._semEnrol = semEnrol
-    }
-}
-
-module.exports = ManualEntry_Key
--- backend/object_models/Blockchain/Unit_Key.js
+++
@@ -1,9 +0,0 @@
-class Unit_Key{
-    constructor (studentId, unitId, semEnrol){
-        this._studentId = studentId
-        this._unitId = unitId
-        this._semEnrol = semEnrol
-    }
-}
-
-module.exports = Unit_Key
