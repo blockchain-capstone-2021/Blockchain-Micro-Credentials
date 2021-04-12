6a28c8de58833b79f6970bd7de54c28e85da73a9
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-12 16:31:17 +1000

    using dirname for the env path

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
 const dotenv = require('dotenv');
 dotenv.config({
-  path: './backend/.env',
+  path: path.join(__dirname, "/backend/.env"),
   debug: process.env.DEBUG
 });
 
