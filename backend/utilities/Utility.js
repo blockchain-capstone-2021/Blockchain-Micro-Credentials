248707330b59c39682310bb27bb11ee942803162
jacktswallow <jack.t.swallow@gmail.com>
2021-04-17 15:30:38 +1000

    Module view controller created, utility file with function for getting current semester created, minor syntax fixes made in DbModuleController and module_attempt

---
+++ backend/utilities/Utility.js
@@ -0,0 +1,18 @@
+async function getCurrentSemester(){
+    let date = new Date()
+    let semOfEnrol = `Y${date.getFullYear}S`
+    let month = date.getMonth()
+    let semester
+    if(month < 7){
+        semester = '1'
+    }else{
+        semester = '2'
+    }
+    semOfEnrol.concat(semester)
+
+    return semOfEnrol
+}
+
+module.exports = {
+    getCurrentSemester
+}
