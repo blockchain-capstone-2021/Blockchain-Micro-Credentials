1f475d59f00fb52c3b795e96d23951f597340abd
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-17 13:28:23 +1000

    completed the unit view controller

---
+++ backend/controllers/Unit_Controller.js
@@ -0,0 +1,13 @@
+const dbUnitController = require('../db/controllers/DbUnitController')
+
+const getUnitsByStaff = async (req, res, next) => {
+    await dbUnitController.getUnitByStaff(req.params.staffId).then(units => {
+        res.locals.units = units
+        
+    });
+    next();
+}
+
+module.exports = {
+    getUnitsByStaff
+}
