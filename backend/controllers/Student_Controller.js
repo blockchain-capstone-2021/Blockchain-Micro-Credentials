149f4924fcc9aece8077d44bf6226e46229a70c5
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-21 17:06:55 +1000

    added error handling to student controller

--- backend/controllers/Student_Controller.js
+++ backend/controllers/Student_Controller.js
@@ -2,25 +2,35 @@ const dbStudentController = require('../db/controllers/DbStudentController')
 const SHA256 = require("crypto-js/sha256");
 
 const submitStudentLogin = async (req, res, next) => {
-    let _studentId = req.params.studentId.toLowerCase();
-    let exists = await dbStudentController.checkStudentExists(_studentId)
-    res.locals.exists = exists;
+    try{
+        let _studentId = req.params.studentId.toLowerCase();
+        let exists = await dbStudentController.checkStudentExists(_studentId)
+        res.locals.exists = exists;
 
-    if(exists)
-    {
-        let student = await dbStudentController.getStudent(_studentId)
-        if(student.passwordHash === SHA256(req.params.password).toString())
+        if(exists)
         {
-            res.locals.loggedIn = true;
+            let student = await dbStudentController.getStudent(_studentId)
+            if(student.passwordHash === SHA256(req.params.password).toString())
+            {
+                res.locals.loggedIn = true;
+            }
+            else
+            {
+                res.locals.loggedIn = false;
+            }
         }
-        else
-        {
-            res.locals.loggedIn = false;
-        }
+
+        res.locals.success = true
     }
-    next();
+    catch(err)
+    {
+        res.locals.success = false
+    }
+    finally{
+        next();
+    }
 }
 
 module.exports = {
     submitStudentLogin
-}
+}   
