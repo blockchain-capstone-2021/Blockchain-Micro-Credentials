2152492c5a2ff2d702cb21abcd1970f0ea2d4072
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-21 17:09:19 +1000

    added error handling to staff controller

--- backend/controllers/Staff_Controller.js
+++ backend/controllers/Staff_Controller.js
@@ -2,23 +2,33 @@ const dbStaffController = require('../db/controllers/DbStaffController')
 const SHA256 = require("crypto-js/sha256");
 
 const submitStaffLogin = async (req, res, next) => {
-    let _staffId = req.params.staffId.toLowerCase();
-    let exists = await dbStaffController.checkStaffExists(_staffId);
-    res.locals.exists = exists
 
-    if(exists)
-    {
-        let staff = await dbStaffController.getStaff(_staffId)
-        if(staff.passwordHash === SHA256(req.params.password).toString())
+    try{
+        let _staffId = req.params.staffId.toLowerCase();
+        let exists = await dbStaffController.checkStaffExists(_staffId);
+        res.locals.exists = exists
+    
+        if(exists)
         {
-            res.locals.loggedIn = true;
+            let staff = await dbStaffController.getStaff(_staffId)
+            if(staff.passwordHash === SHA256(req.params.password).toString())
+            {
+                res.locals.loggedIn = true;
+            }
+            else
+            {
+                res.locals.loggedIn = false;
+            }
         }
-        else
-        {
-            res.locals.loggedIn = false;
-        }
+
+        res.locals.success = true
     }
-    next();
+    catch(err){
+        res.locals.success = false
+    }
+    finally{
+        next();
+    }
 }
 
 module.exports = {
