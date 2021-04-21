3c4d5fa8058b8041d87bd6022a90768af4463698
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-04-21 21:03:08 +1000

    qa contract testing including qa tracker testing

--- backend/blockchain/test/QA_Test.sol
+++ backend/blockchain/test/QA_Test.sol
@@ -4,41 +4,114 @@ pragma solidity >=0.6.8 <=0.8.3;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/QA.sol";
+import "../contracts/QA_Tracker.sol";
 
-contract QA_Test 
+contract QA_Test
 {
     // The address of the Module contract to be tested
-    QA qa_contract = QA(DeployedAddresses.QA());
+    QA qa = QA(DeployedAddresses.QA());
+
+    QA_Tracker qa_tracker = QA_Tracker(DeployedAddresses.QA_Tracker());
 
     // The expected length of the array 
-    uint expectedLength = 1;
+    uint expectedSecondLength = 2;
+
+    uint expectedFirstLength = 1;
+
+    uint expectedSecondIndex = 1;
 
     uint expectedFirstIndex = 0;
 
-    string expectedString = "This is a sample string";
+    string expectedSecondKey = "This is the expected key";
+
+    string expectedFirstKey = "This is a test key";
+
+    string expectedSecondString = "This is a test string";
+
+    string expectedFirstString = "This is a sample string";
 
     function testAddHash() public 
     {
-        qa_contract.addHash(expectedString);
+        qa.addHash(expectedFirstString);
 
-        uint returnedIndex = qa_contract.getLastIndex();
+        uint returnedIndex = qa.getLastIndex();
+
+        qa_tracker.addTracker(expectedFirstKey, returnedIndex);
+
+        uint size = qa_tracker.getLength();
+
+        Assert.equal(size, expectedFirstLength, "Expected size should match the size that is returned.");
 
         Assert.equal(returnedIndex, expectedFirstIndex, "Expected Index should match the index that is returned.");
     }
 
+    function testAddSecondHash() public
+    {
+        qa.addHash(expectedSecondString);
+
+        uint returnedIndex = qa.getLastIndex();
+
+        qa_tracker.addTracker(expectedSecondKey, returnedIndex);
+
+        uint size = qa_tracker.getLength();
+
+        Assert.equal(size, expectedSecondLength, "Expected size should match the size that is returned.");
+
+        Assert.equal(returnedIndex, expectedSecondIndex, "Expected Index should match the index that is returned.");
+    }
+
     function  testLength() public
     {
-        uint returnedLength = qa_contract.getLength();
+        uint returnedLength = qa.getLength();
 
-        Assert.equal(returnedLength, expectedLength, "Expected length should match the length that is returned.");
+        Assert.equal(returnedLength, expectedSecondLength, "Expected length should match the length that is returned.");
     }
 
     function testReturnFirstHash() public
     {
-        string memory returnedHash = qa_contract.returnHash(expectedFirstIndex);
+        uint returnedIndex = qa_tracker.returnIndex(expectedFirstKey);
+
+        string memory returnedHash = qa.returnHash(returnedIndex);
         
-        Assert.equal(returnedHash, expectedString, "Expected string should match the string that is returned.");
+        Assert.equal(returnedHash, expectedFirstString, "Expected string should match the string that is returned.");
+    }
+
+    function testReturnSecondHash() public
+    {
+        uint returnedIndex = qa_tracker.returnIndex(expectedSecondKey);
+
+        string memory returnedHash = qa.returnHash(returnedIndex);
+        
+        Assert.equal(returnedHash, expectedSecondString, "Expected string should match the string that is returned.");
+    }
+
+    function testExists() public
+    {
+        bool exists = qa_tracker.checkExists(expectedFirstKey);
+
+        Assert.equal(exists, true, "Expected state of existence should match the state of existence that is returned.");
+    }
+
+    function testNotExist() public
+    {
+        bool exists = qa_tracker.checkExists("does not exist");
+
+        Assert.equal(exists, false, "Expected state of existence should match the state of existence that is returned.");
+    }
+
+    function testUpdateValue() public
+    {
+        qa_tracker.addTracker(expectedSecondKey, expectedFirstIndex);
+
+        uint returnedIndex = qa_tracker.returnIndex(expectedSecondKey);
+
+        Assert.equal(returnedIndex, expectedFirstIndex, "Expected index should match the index that is returned.");
+
+        string memory returnedHash = qa.returnHash(returnedIndex);
+        
+        Assert.equal(returnedHash, expectedFirstString, "Expected string should match the string that is returned.");
     }
 }
+
 
 
