767841152b26ea90958662ac84379fdb9bdfc2df
Daniel Dominique (s3745409) <42055055+s3745409@users.noreply.github.com>
2021-04-24 13:24:10 +1000

    Added route and fixed Question controller

--- backend/routes/modules.js
+++ backend/routes/modules.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { getUnitModules } = require('../controllers/Module_Controller')
+const { getQuestions } = require('../controllers/Question_Controller')
 var router = express.Router()
 
 
@@ -14,4 +15,18 @@ router.get('/:unitId', getUnitModules, async function (req,res,next) {
         message: "That did not work. Try again."
     })
 })
+
+router.get('/:moduleId/questions', getQuestions, async function (req,res,next) {
+    if(res.locals.success) {
+        return res.status(200).send({
+            success: res.locals.success,
+            questions: res.locals.questions,
+            answersMap: res.locals.answersMap
+        })
+    }
+    return res.status(400).send({
+        message: "There are no questions in the module."
+    }) 
+})
+
 module.exports = router;
