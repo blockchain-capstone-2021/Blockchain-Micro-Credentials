37a75ba2cb2813cf7cad4a9d3fed2f5519063ccd
jacktswallow <jack.t.swallow@gmail.com>
2021-04-28 17:18:13 +1000

    additional seeding added to questions

--- backend/db/seeders/06-module-seed.js
+++ backend/db/seeders/06-module-seed.js
@@ -8,7 +8,7 @@ module.exports = {
     //Security
     for(let i =1; i<=10; i++)
     {
-      randomIndex = Math.floor(Math.random()*3)
+      let randomIndex = Math.floor(Math.random()*3)
       const module = {
         unitId: 'COSC2536',
         moduleName: 'Security in IT Module '+i,
@@ -24,7 +24,7 @@ module.exports = {
     let weightsBlockchain = [5,5,5,5,5,25,5,5,5,5,5,25]
     for(let i =1; i<=12; i++)
     {
-      randomIndex = Math.floor(Math.random()*3)
+      let randomIndex = Math.floor(Math.random()*3)
       const module = {
         unitId: 'INTE2554',
         moduleName: 'Blockchain Applications Module '+i,
@@ -40,7 +40,7 @@ module.exports = {
     let weightsWDT = [10,10,10,20,10,10,10,20]
     for(let i =1; i<=8; i++)
     {
-      randomIndex = Math.floor(Math.random()*3)
+      let randomIndex = Math.floor(Math.random()*3)
       const module = {
         unitId: 'COSC2277',
         moduleName: 'Web Development Technologies Module '+i,
@@ -56,7 +56,7 @@ module.exports = {
     let weightsI3D = [5,5,30,5,5,50]
     for(let i =1; i<=6; i++)
     {
-      randomIndex = Math.floor(Math.random()*3)
+      let randomIndex = Math.floor(Math.random()*3)
       const module = {
         unitId: 'COSC1186',
         moduleName: 'Interactive 3D Graphics Module '+i,
--- backend/db/seeders/07-question-seed.js
+++ backend/db/seeders/07-question-seed.js
@@ -4,9 +4,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     var seedData = [];
-    for(var m=1; m<=10; m++)
+    let potentialQuestionPool = [20,30,40]
+
+    //Security
+    for(let m=1; m<=10; m++)
     {
-      for(let i =1; i<=20; i++)
+      let randomIndex = Math.floor(Math.random()*3)
+      let questionPool = potentialQuestionPool[randomIndex]
+      for(let i =1; i<=questionPool; i++)
       {
         const data = {
           moduleId:m,
@@ -15,6 +20,53 @@ module.exports = {
         seedData.push(data);
       }
     }
+
+    //Blockchain
+    for(let m=11; m<=22; m++)
+    {
+      let randomIndex = Math.floor(Math.random()*3)
+      let questionPool = potentialQuestionPool[randomIndex]
+      for(let i =1; i<=questionPool; i++)
+      {
+        const data = {
+          moduleId:m,
+          content:`This is example question ${i} for module ${m-10}`
+        };
+        seedData.push(data);
+      }
+    }
+
+    //WDT
+    for(let m=23; m<=30; m++)
+    {
+      let randomIndex = Math.floor(Math.random()*3)
+      let questionPool = potentialQuestionPool[randomIndex]
+      for(let i =1; i<=questionPool; i++)
+      {
+        const data = {
+          moduleId:m,
+          content:`This is example question ${i} for module ${m-22}`
+        };
+        seedData.push(data);
+      }
+    }
+
+    //I3D
+    for(let m=31; m<=36; m++)
+    {
+      let randomIndex = Math.floor(Math.random()*3)
+      let questionPool = potentialQuestionPool[randomIndex]
+      for(let i =1; i<=questionPool; i++)
+      {
+        const data = {
+          moduleId:m,
+          content:`This is example question ${i} for module ${m-30}`
+        };
+        seedData.push(data);
+      }
+    }
+
+
     return queryInterface.bulkInsert('Questions', seedData);
   },
   down: (queryInterface, Sequelize) => {
