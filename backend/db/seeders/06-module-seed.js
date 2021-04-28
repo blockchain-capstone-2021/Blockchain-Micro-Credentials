62fe0721e12622f0e5309df78d9c086fbb184629
jacktswallow <jack.t.swallow@gmail.com>
2021-04-28 16:37:03 +1000

    additional seeding added to modules

--- backend/db/seeders/06-module-seed.js
+++ backend/db/seeders/06-module-seed.js
@@ -2,88 +2,70 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Modules', [
-      {
+
+    let potentialNoOfQuestions = [10,15,20]
+    //Security
+    for(let i =1; i<=10; i++)
+    {
+      randomIndex = Math.floor(Math.random()*3)
+      const module = {
         unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 1',
-        moduleNo: 1,
-        noOfQuestions: 10,
+        moduleName: 'Security in IT Module '+i,
+        moduleNo: i,
+        noOfQuestions: potentialNoOfQuestions[randomIndex],
         published: true,
         weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 2',
-        moduleNo: 2,
-        noOfQuestions: 10,
+      };
+      seedData.push(module);
+    }
+
+    //Blockchain
+    let weightsBlockchain = [5,5,5,5,5,25,5,5,5,5,5,25]
+    for(let i =1; i<=12; i++)
+    {
+      randomIndex = Math.floor(Math.random()*3)
+      const module = {
+        unitId: 'INTE2554',
+        moduleName: 'Blockchain Module '+i,
+        moduleNo: i,
+        noOfQuestions: potentialNoOfQuestions[randomIndex],
         published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 3',
-        moduleNo: 3,
-        noOfQuestions: 10,
+        weight: weightsBlockchain[i-1]
+      };
+      seedData.push(module);
+    }
+
+    //WDT
+    let weightsWDT = [10,10,10,20,10,10,10,20]
+    for(let i =1; i<=8; i++)
+    {
+      randomIndex = Math.floor(Math.random()*3)
+      const module = {
+        unitId: 'COSC2277',
+        moduleName: 'Web Development Technologies Module '+i,
+        moduleNo: i,
+        noOfQuestions: potentialNoOfQuestions[randomIndex],
         published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 4',
-        moduleNo: 4,
-        noOfQuestions: 10,
+        weight: weightsWDT[i-1]
+      };
+      seedData.push(module);
+    }
+
+    //I3D
+    let weightsI3D = [5,5,30,5,5,50]
+    for(let i =1; i<=6; i++)
+    {
+      randomIndex = Math.floor(Math.random()*3)
+      const module = {
+        unitId: 'COSC1186',
+        moduleName: 'Interactive 3D Graphics Module '+i,
+        moduleNo: i,
+        noOfQuestions: potentialNoOfQuestions[randomIndex],
         published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 5',
-        moduleNo: 5,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 6',
-        moduleNo: 6,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 7',
-        moduleNo: 7,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 8',
-        moduleNo: 8,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      },
-      {
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 9',
-        moduleNo: 9,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      },
-      { 
-        unitId: 'COSC2536',
-        moduleName: 'Security in IT Module 10',
-        moduleNo: 10,
-        noOfQuestions: 10,
-        published: true,
-        weight: 10
-      }
-    ]);
+        weight: weightsI3D[i-1]
+      };
+      seedData.push(module);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
