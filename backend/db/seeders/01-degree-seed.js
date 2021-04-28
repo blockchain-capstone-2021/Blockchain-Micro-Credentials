d66493cb7010890bb7b4d7980309095466a1d8ff
jacktswallow <jack.t.swallow@gmail.com>
2021-04-28 14:31:51 +1000

    aditional seeding added to degree

--- backend/db/seeders/01-degree-seed.js
+++ backend/db/seeders/01-degree-seed.js
@@ -2,12 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Degrees', [{
-      degreeId: 'BP094',
-      degreeName: 'Bachelor of Computer Science',
-      totalCreditPoints: 288,
-      creditPointsPerSem: 48
-    }]);
+    return queryInterface.bulkInsert('Degrees', [
+      {
+        degreeId: 'BP094',
+        degreeName: 'Bachelor of Computer Science',
+        totalCreditPoints: 288,
+        creditPointsPerSem: 48
+      },
+      {
+        degreeId: 'BBP096',
+        degreeName: 'Bachelor of Software Engineering',
+        totalCreditPoints: 384,
+        creditPointsPerSem: 48
+      }
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
