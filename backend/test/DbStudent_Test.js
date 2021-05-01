773764f0aa537f07f729f8174effe1bc4b2357e5
shreyparekh <49294192+shreyparekh@users.noreply.github.com>
2021-05-01 17:29:00 +1000

    Completed Db Student Test

---
+++ backend/test/DbStudent_Test.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const SequelizeMock = require('sequelize-mock');
+var DBConnectionMock = new SequelizeMock();
+
+const dbStudentController = require('../db/controllers/DbStudentController')
+
+var StudentMock = DBConnectionMock.define('students', {
+    'studentId': 's3710669',
+    'degreeId': 'BP094',
+    'studentName': 'Shrey Parekh',
+    'studentEmail': 's3710669@student.rmit.edu.au',
+    'studentCreditPoints': 252,
+    'passwordHash': 'testingHash',
+});
+
+describe('Db Student Controller', () => {
+    describe('getStudent()', () => {
+        it('should return the correct student Id', async () => {
+            let studentId = 's3710669'
+            let student = await dbStudentController.getStudent(studentId)
+            assert.strictEqual(student.studentId, studentId);
+        }).timeout(10000);
+        it('should return the incorrect student Id', async () => {
+            let studentId = 's3710669'
+            let expectedStudentId = "s3541003"
+            let student = await dbStudentController.getStudent(studentId)
+            assert.notStrictEqual(student.studentId, expectedStudentId);
+        }).timeout(10000);
+    })
+    describe('checkStudentExists()', () => {
+        it('should return the student exists', async () => {
+            let studentId = 's3710669'
+            let expectedExists = true
+            let exists = await dbStudentController.checkStudentExists(studentId)
+            assert.strictEqual(exists, expectedExists);
+        }).timeout(10000);
+        it('should return the student does not exist', async () => {
+            let studentId = 'sTest'
+            let expectedExists = false
+            let exists = await dbStudentController.checkStudentExists(studentId)
+            assert.strictEqual(exists, expectedExists);
+        }).timeout(10000);
+    })
+    describe('updateCreditPoints()', () => {
+        it('should return the correct credit points', async () => {
+            let expectedCreditPoints = 264
+
+            await StudentMock.update({ studentCreditPoints: 264}, {
+                where: {
+                    studentId: "s3710669"
+                }
+            });
+
+            StudentMock.findOne({
+                where: {
+                    studentId: "s3710669"
+                }
+            }).then(function (student) {
+                assert.strictEqual(student.get('studentCreditPoints'), expectedCreditPoints);
+            })
+            
+        }).timeout(10000);
+    })
+});
