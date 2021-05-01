9634c0d5d620854f88a91d9de915b20404e08e17
shreya <shreysaman@gmail.com>
2021-05-01 14:25:40 +1000

    Created student profile page

--- frontend/src/components/student/StudentProfile.js
+++ frontend/src/components/student/StudentProfile.js
@@ -1,26 +1,44 @@
 import React, {useState, useEffect} from 'react'
 import microcredapi from '../../apis/microcredapi'
 
-function StudentProfile() {
+const StudentProfile = (props) => {
+
+    const [student, setStudent] = useState()
 
     useEffect(() => {
 
         async function getStudent() {
             const response = await microcredapi.get(`student/${window.localStorage.getItem('userId')}`)
-            console.log(response.data);
+            console.log(response.data)
+            setStudent({'name': response.data.student.studentName, 'id': response.data.student.studentId, 'email': response.data.student.studentEmail, 'degree': Object.values(response.data.degreeName)[1], 'credits': response.data.student.studentCreditPoints})
         }
         getStudent()
-
     }, [])
+    console.log(student)
 
     return (
-        <div>
-            
+        <div className="container mt-5">
+                {
+                    student?
+                    <section>
+                        <h1>Hello, {student.name}!</h1>
+                        <form>
+                        <p>Student ID:</p>  
+                        <input type='text' disabled = {true} defaultValue={student.id} />
+                        <p>Email:</p> 
+                        <input type='email' disabled = {true} defaultValue={student.email} />
+                        <p>Degree:</p> 
+                        <input type='text' disabled = {true} defaultValue={student.degree} />
+                        <p>Credits:</p> 
+                        <input type='text' disabled = {true} defaultValue={student.credits} />
+                        </form>
+                        
+                    </section>:
+                    ""
+                }
+
         </div>
     )
 }
-
-
-
 
 export default StudentProfile
