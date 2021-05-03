1c6ab24049517753b5209e210f5a9aec7501d694
Daniel Dominique (s3745409) <42055055+s3745409@users.noreply.github.com>
2021-05-03 17:28:54 +1000

    Shifted bottons on Question manage page to left

--- frontend/src/components/questions/StaffQuestionManage.js
+++ frontend/src/components/questions/StaffQuestionManage.js
@@ -89,7 +89,10 @@ const StaffQuestionManage = () => {
                 <td>{_question.questionId}</td>
                 <td>{_question.moduleId}</td>
                 <td>{_question.content}</td>
-                <td>
+                <td class="d-flex">
+                  <div style={{marginLeft: 'auto', marginRight: '1em'}}>
+
+                  </div>
                 <Link to={`/question/${_question.questionId}`} class="btn btn-warning mx-1">View</Link>
                 <button type="button" className="btn btn-danger mx-2" data-bs-questionid={_question.questionId} data-bs-moduleid={_question.moduleId} data-bs-toggle="modal"  data-bs-target="#deleteConf" onClick={displayDeleteModal('DELETE', history, redirect)}>
                     Delete
