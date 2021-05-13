c103be88ee138306e82663b427a1c5f6724be64f
jacktswallow <jack.t.swallow@gmail.com>
2021-05-13 17:42:41 +1000

    DB Answer test file completed

--- backend/test/DbAnswer_Test.js
+++ backend/test/DbAnswer_Test.js
@@ -2,36 +2,31 @@ const assert = require('assert');
 
 const sequelizeMock = require('sequelize-mock')
 var DBConnectionMock = new sequelizeMock
+var DbAnswerController = require('../db/controllers/DbAnswerController')
 
 var AnswerMock = DBConnectionMock.define('answers', 
-    {
-        questionId: 1,
-        content: "testAnswer1",
-        isCorrect: false
-    }
-)
+{
+    questionId: 1,
+    content: "testAnswer1",
+    isCorrect: false
+}, {autoQueryfallback: false, stopPropagation: false});
+
+AnswerMock.$queueResult(AnswerMock.build({
+    questionId: 1,
+    content: "testAnswer1",
+    isCorrect: false
+}));
 
 describe('DbAnswerController', () => {
-    // describe('getAnswers()', () =>{
-    //     it('should return a list of four answer objects', async () => {
-    //         let questionId = 1
-    //         let answers = await getAnswers(questionId)
-    //         console.log(answers)
-    //         assert.strictEqual(answers.length, 4)
-    //         for(const answer of answers){
-    //             assert.strictEqual(answer.questionId, questionId)
-    //         }
-    //     }).timeout(10000);
-    // })
 
-    // describe('getAnswer()', () =>{
-    //     it('should return the answer with the correct answerId', async () => {
-    //         let expectedAnswerId = 1
-    //         let actualAnswer = await getAnswer(expectedAnswerId)
-    //         let actualAnswerId = actualAnswer.answerId
-    //         assert.strictEqual(actualAnswerId, expectedAnswerId)
-    //     }).timeout(10000);
-    // })
+    describe('getAnswer()', () =>{
+        it('should return the answer with the correct answerId', async () => {
+            let expectedAnswerId = 1
+            let actualAnswer = await getAnswer(expectedAnswerId)
+            let actualAnswerId = actualAnswer.id
+            assert.strictEqual(actualAnswerId, expectedAnswerId)
+        }).timeout(10000);
+    })
 
     describe('getCorrectAnswer()', () =>{
         it('should return the correct answer for a given questionId', async () => {
@@ -49,12 +44,10 @@ describe('DbAnswerController', () => {
             let content = "testAnswer6"
             let isCorrect = false
             answer = await addAnswerToQuestion(questionId, content, isCorrect)
-            console.log(answer.id)
             assert.strictEqual(questionId, answer.questionId)
             assert.strictEqual(content, answer.content)
             assert.strictEqual(isCorrect, answer.isCorrect)
         })
-
     })
   
 })
@@ -123,21 +116,4 @@ async function addAnswerToQuestion(_questionId, _content, _isCorrect)
     });
 
     return instance
-}
-
-//delete answers by question id
-async function deleteAnswersOfQuestion(_questionId)
-{
-    await AnswerMock.destroy({
-        where: {
-            questionId: _questionId
-        }
-    })
-}
-
-async function deleteAllAnswers(questionIdentifiers){
-  for(const id of questionIdentifiers)
-  {
-    await deleteAnswersOfQuestion(id)
-  }
 }
