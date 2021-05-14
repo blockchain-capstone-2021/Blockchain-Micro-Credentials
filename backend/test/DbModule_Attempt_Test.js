a2671db90f71b719a0da861f1e1363c14f78d78f
jacktswallow <jack.t.swallow@gmail.com>
2021-05-14 16:23:34 +1000

    DB module attempt test file completed

---
+++ backend/test/DbModule_Attempt_Test.js
@@ -0,0 +1,79 @@
+const assert = require('assert');
+
+const sequelizeMock = require('sequelize-mock')
+var DBConnectionMock = new sequelizeMock
+
+var Module_AttemptMock = DBConnectionMock.define('module_attempts', {autoQueryfallback: false, stopPropagation: false});
+
+Module_AttemptMock.$queueResult(Module_AttemptMock.build({
+    studentId: "s3541003",
+    moduleId: 1,
+    semOfEnrolment: "Y2021S1",
+    attemptNo: 1
+}));
+
+Module_AttemptMock.$queueResult(Module_AttemptMock.build({
+    studentId: "s3541003",
+    moduleId: 2,
+    semOfEnrolment: "Y2021S1",
+    attemptNo: 1
+}));
+
+
+describe('Enrolment', () => {
+
+    describe('getNoOfAttempts()', () =>{
+        it('should return the correct number of attempts for the given parameters', async () => {
+            let expectedAttemptNo = 1
+            let moduleId = 1
+            let studentId = "s3541003"
+            let sem = "Y2021S1"
+            let actualAttemptNo = await getNoOfAttempts(studentId, moduleId, sem)
+            assert.strictEqual(actualAttemptNo, expectedAttemptNo)
+        }).timeout(10000);
+    })
+
+    describe('checkAttemptsExist()', () =>{
+        it('should return an attempt matching the expected student and module id and semester', async () => {
+            let expectedStudentId = "s3541003"
+            let moduleId = 2
+            let sem = "Y2021S1"
+            let actualAttempt = await checkAttemptsExist(moduleId, sem)
+            assert.strictEqual(actualAttempt.studentId, expectedStudentId)
+            assert.strictEqual(actualAttempt.moduleId, moduleId)
+            assert.strictEqual(actualAttempt.semOfEnrolment, sem)
+        }).timeout(10000);
+    })
+  
+})
+
+//return number of attempts for a given studentId and moduleId
+async function getNoOfAttempts(_studentId, _moduleId, _semOfEnrolment) 
+{
+    let attempts = 0;
+
+    let module_attempt = await Module_AttemptMock.findOne({
+        where: {
+            studentId: _studentId,
+            moduleId: _moduleId,
+            semOfEnrolment: _semOfEnrolment
+        }
+    })
+
+    if(module_attempt){
+        attempts = module_attempt.attemptNo
+    }
+
+    return attempts;
+}
+
+async function checkAttemptsExist(_moduleId, _semOfEnrolment)
+{
+    let module_attempt = await Module_AttemptMock.findOne({
+        where: {
+            moduleId: _moduleId,
+            semOfEnrolment: _semOfEnrolment
+        }
+    })
+    return module_attempt
+}
