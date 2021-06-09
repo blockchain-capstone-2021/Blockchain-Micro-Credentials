c4a2311a1551b313c0c21c58e2c8ba296c0985e6
Daniel Dominique (s3745409) <42055055+s3745409@users.noreply.github.com>
2021-06-09 17:51:42 +1000

    Updated how quiz answers are displayed

--- frontend/src/components/modules/Answer.js
+++ frontend/src/components/modules/Answer.js
@@ -4,18 +4,23 @@ import React from 'react'
 
 const Answer = (props) => {
 
-  function renderAnswers() {
-    return props.answers.map(answer => {
-      return (
-        <div className="form-check py-1" key={answer.answerId}>
-        <input className="form-check-input" type="radio" name={`qid_${answer.questionId}`} id={`q${answer.questionId}_a${answer.answerId}_${answer.isCorrect}`} value="option2"/>
-        <label className="form-check-label" htmlFor={`q${answer.questionId}_a${answer.answerId}_${answer.isCorrect}`}>
-          {answer.content}
-        </label>
-      </div>
-      )
-    })
-  }
+    //render the answers for a given question
+    function renderAnswers() {
+        return props.answers.map(answer => {
+            return (
+            <div class={`alert alert-secondary`} role="alert">
+                <div className="form-check py-1" key={answer.answerId}>
+                    <input className="form-check-input" type="radio" name={`qid_${answer.questionId}`} id={`q${answer.questionId}_a${answer.answerId}_${answer.isCorrect}`} value="option2"/>
+                    <label className="form-check-label" htmlFor={`q${answer.questionId}_a${answer.answerId}_${answer.isCorrect}`}>
+                     {answer.content}
+                    </label>
+                </div>
+            </div>
+            )
+        })
+    }
+
+
 
   return (
     <div>
